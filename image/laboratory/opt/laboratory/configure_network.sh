b285e47d04b0ac59e9f50f2f8b9baf081a1b4dc5
ronserruya <ron.serruya@kik.com>
2018-05-16 16:40:16 +0300

    fix and pull laboratory image

--- image/laboratory/opt/laboratory/configure_network.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -e
-
-# Edit the network.js file to change the network used
-sed -i "s@https://horizon-testnet.stellar.org@$HORIZON_ENDPOINT_TESTNET@g" /opt/laboratory/src/constants/network.js
-sed -i "s@Networks.TESTNET@'$NETWORK_PASSPHRASE_TESTNET'@g" /opt/laboratory/src/constants/network.js
-
-# Edit the accountCreator.js file to change the friendbot service used
-sed -i "s@https://friendbot.stellar.org@$FRIENDBOT_ENDPOINT@g" /opt/laboratory/src/actions/accountCreator.js
