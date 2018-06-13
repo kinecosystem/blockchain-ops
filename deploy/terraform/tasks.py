8bdb797fc0d55a0dd6aa6ade896c10c0b0a3be9d
Ory Band <oryband@gmail.com>
2018-06-13 16:40:08 +0300

    tasks file moved to deploy/terraform, update tasks.py to match

--- deploy/terraform/tasks.py
+++ deploy/terraform/tasks.py
@@ -7,43 +7,43 @@ import yaml
 
 
 TERRAFORM_VERSION = '0.11.7'
+
+
 @task
 def install(c, version=TERRAFORM_VERSION):
     file = f'terraform_{version}_linux_amd64.zip'
 
-    with c.cd('deploy/terraform'):
-        if os.path.exists('terraform'):
-            print('Terraform file found')
-            return
+    if os.path.exists('terraform'):
+        print('Terraform file found')
+        return
 
-        print(f'Downloading Terraform {version}')
-        c.run(f'wget -q https://releases.hashicorp.com/terraform/{version}/{file}')
-        c.run(f'unzip {file}')
-        c.run(f'rm {file}')
+    print(f'Downloading Terraform {version}')
+    c.run(f'wget -q https://releases.hashicorp.com/terraform/{version}/{file}')
+    c.run(f'unzip {file}')
+    c.run(f'rm {file}')
 
 
 @task
-def init(c):
-    with c.cd('deploy/terraform'):
-        c.run('./terraform init')
+def init(c): c.run('./terraform init')
 
 
 @task
 def modules(c):
     print('Downloading Terraform modules')
-    with c.cd('deploy/terraform'):
-        c.run('./terraform get')
+    c.run('./terraform get')
 
 
 @task
-def plan(c):
-    with c.cd('deploy/terraform'):
-        c.run('./terraform plan')
+def plan(c): c.run('./terraform plan')
+
 
 @task
-def apply(c):
-    with c.cd('deploy/terraform'):
-        c.run('./terraform apply')
+def apply(c): c.run('./terraform apply')
+
+
+@task
+def destroy(c): c.run('./terraform destroy')
+
 
 @task
 def template(c,
