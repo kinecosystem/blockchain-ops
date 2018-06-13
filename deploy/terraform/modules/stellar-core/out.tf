e7db5f7fac8589b77f9d3d540fbf3d580d0825e1
Ory Band <oryband@gmail.com>
2018-06-13 18:15:37 +0300

    add stellar-core module output file

---
+++ deploy/terraform/modules/stellar-core/out.tf
@@ -0,0 +1,14 @@
+output "instance_route53_fqdn" {
+  description = "Route53 FQDN name assigned to the EC2 instance"
+  value       = "${aws_route53_record.this.fqdn}"
+}
+
+output "instance_tags" {
+  description = "List of tags"
+  value       = "${module.ec2.tags}"
+}
+
+output "s3" {
+  description = "S3 bucket name"
+  value       = "${aws_s3_bucket.this.id}"
+}
