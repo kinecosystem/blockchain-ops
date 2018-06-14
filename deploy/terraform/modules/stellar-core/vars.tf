ea8772d91b8898b05e8460cc58f7b0dbbfd77375
Ory Band <oryband@gmail.com>
2018-06-14 18:43:20 +0300

    update local.name (name used everywhere) in stellar-core module

--- deploy/terraform/modules/stellar-core/vars.tf
+++ deploy/terraform/modules/stellar-core/vars.tf
@@ -10,5 +10,5 @@ variable "tld" {}
 
 locals {
   # used to name ec2 and rds instances, security groups, etc.
-  name = "stellar-core-${var.zone}-${var.stellar_network_name}"
+  name = "stellar-core-${var.stellar_network_name}-${var.zone}"
 }
