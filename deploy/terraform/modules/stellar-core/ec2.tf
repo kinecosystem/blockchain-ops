bdafe91df993e64c0acab920044b58a834e6140b
Ory Band <oryband@gmail.com>
2018-06-17 15:42:12 +0300

    merge stellar-core/data.tf into ec2.tf
    all data resources relate to it anyways

--- deploy/terraform/modules/stellar-core/ec2.tf
+++ deploy/terraform/modules/stellar-core/ec2.tf
@@ -63,3 +63,33 @@ module "security-group" {
     },
   ]
 }
+
+# data sources to get vpc, subnet, ami, route53 details
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnet" "default" {
+  vpc_id            = "${data.aws_vpc.default.id}"
+  availability_zone = "${var.zone}"
+  default_for_az    = true
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  owners = ["099720109477"] # canonical/ubuntu
+
+  filter {
+    name = "name"
+
+    values = [
+      "ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*",
+    ]
+  }
+}
+
+data "aws_route53_zone" "kin" {
+  name = "kininfrastructure.com."
+}
