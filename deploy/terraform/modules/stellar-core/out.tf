0254d3b5f1922b39d52a40787f894c073a8842ae
Ory Band <oryband@gmail.com>
2018-06-19 13:39:53 +0300

    remove intsance tags output, irrelevant

--- deploy/terraform/modules/stellar-core/out.tf
+++ deploy/terraform/modules/stellar-core/out.tf
@@ -3,11 +3,6 @@ output "route53" {
   value       = "${aws_route53_record.this.fqdn}"
 }
 
-output "instance_tags" {
-  description = "List of tags"
-  value       = "${module.ec2.tags}"
-}
-
 output "s3" {
   description = "S3 bucket name"
   value       = "${aws_s3_bucket.this.id}"
