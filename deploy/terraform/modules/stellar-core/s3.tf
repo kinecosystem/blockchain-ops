de2b2652d406e608f1a5472b3283185b5c815f8a
Ory Band <oryband@gmail.com>
2018-06-20 14:41:48 +0300

    add s3 bucket policy to have public read access

--- deploy/terraform/modules/stellar-core/s3.tf
+++ deploy/terraform/modules/stellar-core/s3.tf
@@ -1,6 +1,22 @@
 resource "aws_s3_bucket" "this" {
   bucket = "${var.name}"
-  acl    = "public-read"
+  policy = "${data.aws_iam_policy_document.ec2_s3_bucket_public_read_access.json}"
+
+  # https://docs.aws.amazon.com/AmazonS3/latest/dev/acl-overview.html
+  acl = "public-read"
+}
+
+data "aws_iam_policy_document" "ec2_s3_bucket_public_read_access" {
+  statement {
+    effect    = "Allow"
+    actions   = ["s3:GetObject"]
+    resources = ["arn:aws:s3:::${var.name}/*"]
+
+    principals = {
+      type        = "*"
+      identifiers = ["*"]
+    }
+  }
 }
 
 output "s3" {
