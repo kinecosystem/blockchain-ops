5aba06972f88534bffef07afa91f24f9c29ecc24
ronserruya <ron.serruya@kik.com>
2018-06-27 15:23:58 +0300

    update health check

---
+++ deploy/health_check/README.md
@@ -0,0 +1,5 @@
+install with  
+```pipenv install```
+
+run with  
+```pipenv run gunicorn -b 0.0.0.0:port main:app```
--- deploy/health_check/main.py
+++ deploy/health_check/main.py
@@ -1,71 +1,83 @@
+"""
+Health check for Horizon.
+
+Healthy: Corresponding stellar-core is 'Synced'
+Unhealthy: Failed/Total requests is > 0.1
+"""
+import json
+import time
+
 import requests
-import yaml
 from flask import Flask
 from flask_cors import CORS
 
+import config
+
+
 app = Flask(__name__)
 CORS(app)
+start_timestamp = time.time()
 
 # For 'health', true is healthy
 health = True
-CORE_URL = ''
 
 
-def check_core(core_url):
+def check_core():
     # Check if the stellar core is synced
     global health
     try:
-        r = requests.get(core_url+'/info')
+        r = requests.get(config.CORE_INFO_URL)
         r.raise_for_status()
 
         health = (r.json()['info']['state'] == 'Synced!')
-    except:
+        if health:
+            return make_reply('Core is synced', 200)
+        return make_reply('Core is not synced', 503)
+    except Exception as e:
         health = False
+        return make_reply('Could not check core: {}'.format(str(e)), 503)
 
 
 def check_horizon():
-    # Check if the ratio of success/failed requests is < 0.1
+    # Check if the ratio of failed/total requests is < 0.1
     global health
     try:
-        r = requests.get('http://horizon-kik.kininfrastructure.com/metrics')
+        r = requests.get(config.HORIZON_METRICS_URL)
         r.raise_for_status()
 
-        total_requests = r.json()['requests.total']['1m.rate']
-        failed_requests = r.json()['requests.failed']['1m.rate']
+        data = r.json()
+        total_requests = data['requests.total']['1m.rate']
+        failed_requests = data['requests.failed']['1m.rate']
         ratio = failed_requests / total_requests
 
-        health = (ratio < 0.1)
-    except:
+        health = (ratio < config.MAX_REQUEST_RATIO)
+        if health:
+            return make_reply('Horizon failed requests ratio is {} < {}'
+                              .format(ratio, config.MAX_REQUEST_RATIO), 200)
+        return make_reply('Horizon failed requests ratio is {} > {}'
+                          .format(ratio, config.MAX_REQUEST_RATIO), 503)
+    except Exception as e:
         health = False
+        return make_reply('Could not check horizon: {}'.format(str(e)), 503)
+
+
+def make_reply(msg, code):
+    # Create a json reply for /status
+    reply = {
+        'status': 'Healthy' if code == 200 else 'Unhealthy',
+        'description': msg,
+        'start_timestamp': start_timestamp,
+        'build': config.BUILD_VER
+    }
+
+    return json.dumps(reply), code
 
 
 @app.route("/status")
 def status():
     # If healthy, check horizon, if unhealthy, check core
     global health
-    global CORE_URL
     if health:
-        check_horizon()
+        return check_horizon()
     else:
-        check_core(CORE_URL)
-
-    if health:
-        return 'Healthy', 200
-    return 'Unhealthy', 503
-
-
-def get_core_url():
-    # Get the stellar-core url from the configuration file that is already on the machine
-    with open('/home/ubuntu/docker-compose.yml', 'r') as stream:
-            data = yaml.safe_load(stream)
-    return data['services']['horizon']['environment']['STELLAR_CORE_URL']
-
-
-def main():
-    global CORE_URL
-    CORE_URL = get_core_url()
-    app.run('0.0.0.0', 4000)
-
-
-if __name__ == '__main__':
-    main()
+        return check_core()
