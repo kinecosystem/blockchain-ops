dc4201a2eec2985338faef20ea8dd13288563ee5
Ory Band <oryband@gmail.com>
2018-08-20 13:03:31 +0300

    update horizon version in ansible role and docker build image

--- images/dockerfiles/horizon-build.sh
+++ images/dockerfiles/horizon-build.sh
@@ -5,8 +5,9 @@ set -x
 set -e
 
 BUILD_TIME=$(date -u --rfc-3339=seconds | sed 's/ /T/')
+HORIZON_VERSION="kinecosystem/horizon-v0.12.3-control-db-connection-count"
 
 sudo docker run \
     -v $GOPATH/src/github.com/kinecosystem/go:/go/src/github.com/kinecosystem/go \
     kinecosystem/horizon-build \
-    bash -c "go build -ldflags='-X \"github.com/kinecosystem/go/support/app.buildTime=$BUILD_TIME\" -X \"github.com/kinecosystem/go/support/app.version=kinecosystem/horizon-v0.12.3-psql-optimization\"' -o ./horizon ./services/horizon"
+    bash -c "go build -ldflags='-X \"github.com/kinecosystem/go/support/app.buildTime=$BUILD_TIME\" -X \"github.com/kinecosystem/go/support/app.version=${HORIZON_VERSION}\"' -o ./horizon ./services/horizon"
