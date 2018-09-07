dad7fac937b9931d0b3950672fef6102f77aca1e
Ory Band <oryband@gmail.com>
2018-09-07 16:58:27 +0300

    image/horizon-build.sh also builds docker image and pushes to dockerhub
    also update horizon version in ansisble

--- images/dockerfiles/horizon-build.sh
+++ images/dockerfiles/horizon-build.sh
@@ -5,9 +5,21 @@ set -x
 set -e
 
 BUILD_TIME=$(date -u --rfc-3339=seconds | sed 's/ /T/')
-HORIZON_VERSION="kinecosystem/horizon-v0.12.3-sse-hello-timer-5s"
+HORIZON_VERSION='kinecosystem/horizon-v0.12.3-sse-preamble-only-when-no-error'
+HORIZON_IMAGE_TAG='v0.12.3-sse-preamble-only-when-no-error'
 
 sudo docker run \
     -v $GOPATH/src/github.com/kinecosystem/go:/go/src/github.com/kinecosystem/go \
     kinecosystem/horizon-build \
     bash -c "go build -ldflags='-X \"github.com/kinecosystem/go/support/app.buildTime=$BUILD_TIME\" -X \"github.com/kinecosystem/go/support/app.version=${HORIZON_VERSION}\"' -o ./horizon ./services/horizon"
+
+mv -f $GOPATH/src/github.com/kinecosystem/go/horizon ./
+
+sudo docker build \
+    -f Dockerfile.horizon-bin \
+    -t kinecosystem/horizon:latest \
+    -t kinecosystem/horizon:${HORIZON_IMAGE_TAG} \
+    .
+
+sudo docker push kinecosystem/horizon:${HORIZON_IMAGE_TAG}
+sudo docker push kinecosystem/horizon:latest
