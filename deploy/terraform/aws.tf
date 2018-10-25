f348a51b7ad981af9c4b04b4e35b32c8e5c4e297
Ory Band <oryband@gmail.com>
2018-10-25 18:53:40 +0300

    move common aws providers to their own .tf file
    avoids a bug where destroying instances breaks terraform
    because it can't find the providers

---
+++ deploy/terraform/aws.tf
@@ -0,0 +1,20 @@
+# define one provider per region
+provider "aws" {
+  alias  = "us-east-1"
+  region = "us-east-1"
+}
+
+provider "aws" {
+  alias  = "us-east-2"
+  region = "us-east-2"
+}
+
+provider "aws" {
+  alias  = "us-west-1"
+  region = "us-west-1"
+}
+
+provider "aws" {
+  alias  = "us-west-2"
+  region = "us-west-2"
+}
