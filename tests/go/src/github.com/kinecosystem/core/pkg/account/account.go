78b99fa0cab1a4a8d2aed9622fbb5e443c81cf70
Ory Band <oryband@gmail.com>
2018-11-26 18:17:46 +0200

    mv pkg/account submit function to its owk package
    under pkg/submit

--- tests/go/src/github.com/kinecosystem/core/pkg/account/account.go
+++ tests/go/src/github.com/kinecosystem/core/pkg/account/account.go
@@ -5,27 +5,17 @@ package account
 
 import (
 	"math"
-	"net/http"
-	"time"
 
 	"github.com/go-kit/kit/log"
 	"github.com/go-kit/kit/log/level"
 	"github.com/stellar/go/build"
-	"github.com/stellar/go/clients/horizon"
 	"github.com/stellar/go/keypair"
 
 	"github.com/kinecosystem/core/pkg/errors"
+	"github.com/kinecosystem/core/pkg/submit"
 )
 
-const (
-	maxOpsInTx = 100
-
-	fundTimeout = 10 * time.Second
-	getTimeout  = fundTimeout
-
-	submitTimeout       = 20 * time.Second
-	retryFailedTxAmount = 10
-)
+const maxOpsInTx = 100
 
 type Keypair struct {
 	Seed    string `json:"seed"`
@@ -62,7 +52,7 @@ func Create(horizonAddr string, network build.Network, funder *keypair.Full, acc
 
 		level.Info(logger).Log("msg", "submitting create account transaction")
 
-		err := submitWithRetry(horizonAddr, network, ops, funder.Seed(), logger)
+		err := submit.SubmitWithRetry(horizonAddr, network, ops, funder.Seed(), logger)
 		if err != nil {
 			errors.GetTxErrorResultCodes(err, logger)
 			return nil, err
@@ -78,59 +68,4 @@ func Create(horizonAddr string, network build.Network, funder *keypair.Full, acc
 	}
 
 	return keypairs, nil
-}
-
-func submitWithRetry(horizonAddr string, network build.Network, ops []build.TransactionMutator, seed string, logger log.Logger) error {
-	var err error
-	for i := 0; i < retryFailedTxAmount; i++ {
-		level.Info(logger).Log("retry_index", i, "msg", "submitting transaction")
-
-		client := horizon.Client{
-			URL:  horizonAddr,
-			HTTP: &http.Client{Timeout: submitTimeout}}
-
-		fullOps := append(
-			[]build.TransactionMutator{
-				build.SourceAccount{AddressOrSeed: seed},
-				network,
-				build.AutoSequence{
-					SequenceProvider: &horizon.Client{
-						URL:  horizonAddr,
-						HTTP: client.HTTP,
-					},
-				},
-			},
-			ops...,
-		)
-
-		txBuilder, err := build.Transaction(fullOps...)
-		if err != nil {
-			level.Error(logger).Log("msg", err)
-			return err
-		}
-
-		txEnv, err := txBuilder.Sign(seed)
-		if err != nil {
-			level.Error(logger).Log("msg", err)
-			return err
-		}
-
-		var txEnvB64 string
-		txEnvB64, err = txEnv.Base64()
-		if err != nil {
-			level.Error(logger).Log("msg", err)
-			return err
-		}
-
-		_, err = client.SubmitTransaction(txEnvB64)
-		if err == nil {
-			return nil
-		}
-
-		errors.GetTxErrorResultCodes(err, logger)
-
-		time.Sleep(5 * time.Second)
-	}
-
-	return err
 }
---
+++ tests/go/src/github.com/kinecosystem/core/pkg/submit/submit.go
@@ -0,0 +1,74 @@
+// Package submit implements submitting transactions to Horizon.
+package submit
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/go-kit/kit/log"
+	"github.com/go-kit/kit/log/level"
+	"github.com/stellar/go/build"
+	"github.com/stellar/go/clients/horizon"
+
+	"github.com/kinecosystem/core/pkg/errors"
+)
+
+const (
+	submitTimeout       = 20 * time.Second
+	retryFailedTxAmount = 10
+)
+
+func SubmitWithRetry(horizonAddr string, network build.Network, ops []build.TransactionMutator, seed string, logger log.Logger) error {
+	var err error
+	for i := 0; i < retryFailedTxAmount; i++ {
+		level.Info(logger).Log("retry_index", i, "msg", "submitting transaction")
+
+		client := horizon.Client{
+			URL:  horizonAddr,
+			HTTP: &http.Client{Timeout: submitTimeout}}
+
+		fullOps := append(
+			[]build.TransactionMutator{
+				build.SourceAccount{AddressOrSeed: seed},
+				network,
+				build.AutoSequence{
+					SequenceProvider: &horizon.Client{
+						URL:  horizonAddr,
+						HTTP: client.HTTP,
+					},
+				},
+			},
+			ops...,
+		)
+
+		txBuilder, err := build.Transaction(fullOps...)
+		if err != nil {
+			level.Error(logger).Log("msg", err)
+			return err
+		}
+
+		txEnv, err := txBuilder.Sign(seed)
+		if err != nil {
+			level.Error(logger).Log("msg", err)
+			return err
+		}
+
+		var txEnvB64 string
+		txEnvB64, err = txEnv.Base64()
+		if err != nil {
+			level.Error(logger).Log("msg", err)
+			return err
+		}
+
+		_, err = client.SubmitTransaction(txEnvB64)
+		if err == nil {
+			return nil
+		}
+
+		errors.GetTxErrorResultCodes(err, logger)
+
+		time.Sleep(5 * time.Second)
+	}
+
+	return err
+}
