9f41dd3a828588523dd595f8f54e5429350972de
ordemri <ordemri@gmail.com>
2018-12-02 18:54:32 +0200

    additional changes regarding pr87

--- tests/python/helpers.py
+++ tests/python/helpers.py
@@ -1,6 +1,6 @@
 import asyncio
 from hashlib import sha256
-
+from kin.blockchain.builder import Builder
 from kin import Keypair
 from kin_base import Keypair as BaseKeypair
 
@@ -26,3 +26,20 @@ async def send_txs(txs):
 def get_latest_ledger(client):
     params = {'order': 'desc', 'limit': 1}
     return client.horizon.ledgers(params=params)['_embedded']['records'][0]
+
+def create_whitelisted_account(client, wlSecret, account):
+    builder = Builder('LOCAL', client.horizon, fee=client.get_minimum_fee(), secret=wlSecret)
+    builder.get_sequence()
+    builder.append_manage_data_op(account.public_address, account._hint)
+    builder.sign()
+    builder.submit()
+
+
+def remove_whitelisted_account(client, wlSecret, account):
+    builder = Builder('LOCAL', client.horizon, fee=client.get_minimum_fee(), secret=wlSecret)
+    builder.get_sequence()
+    builder.append_manage_data_op(account.public_address, ''.encode())
+    builder.sign()
+    builder.submit()
+
+
