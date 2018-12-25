ed4f90f91ed2611bebaf29db89f1f2c2330e2206
fuzzyami <ami.blonder@kik.com>
2018-12-25 12:59:46 +0200

    add a whole bunch of tools for working with the core: dump db, dump logs, set log level

---
+++ deploy/ansible/playbooks/roles/stellar-core/templates/scripts/set_core_log_level.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+case "$1" in
+        'debug')
+        log_level='debug'
+        echo 'setting logs to DEBUG'
+        ;;
+
+        'info')
+        log_level='info'
+        echo 'setting logs to INFO'
+        ;;
+
+        *)
+            echo $"Usage: set_core_log_level {debug|info}"
+            exit 1
+esac
+curl 'http://localhost:11626/ll?level='$log_level
+echo 'current log level:'
+curl 'http://localhost:11626/ll?level'
