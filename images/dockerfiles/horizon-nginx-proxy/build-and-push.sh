deb7f52074b017e8b3af3936df24ef38905695c9
fuzzyami <ami.blonder@kik.com>
2019-01-02 13:30:48 +0200

    update nginx version

--- images/dockerfiles/horizon-nginx-proxy/build-and-push.sh
+++ images/dockerfiles/horizon-nginx-proxy/build-and-push.sh
@@ -1,2 +1,2 @@
-docker build . -t kinecosystem/horizon-nginx-statsd:v1.0.5
-docker push kinecosystem/horizon-nginx-statsd:v1.0.5
+docker build . -t kinecosystem/horizon-nginx-statsd:v1.0.6
+docker push kinecosystem/horizon-nginx-statsd:v1.0.6
