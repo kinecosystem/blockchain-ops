0b49d35891c155b4d3f4fb3647461d874f49e607
Ory Band <oryband@gmail.com>
2019-01-03 16:17:31 +0200

    control avg block time (delay between spam rounds) in stress test

--- tests/python/stress.py
+++ tests/python/stress.py
@@ -8,9 +8,6 @@ import time
 from helpers import send_txs_multiple_endpoints
 
 
-AVG_BLOCK_TIME = 5  # seconds
-
-
 logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s %(message)s')
 
 
@@ -23,6 +20,7 @@ def parse_args():
     parser.add_argument('--endpoint', required=True, action='append',
                         help='Endpoint URLs for submitting transactions (use multiple --endpoint flags for multiple addresses)')
     parser.add_argument('--submit-to-core', action='store_true', help='Submit to Core instead of Horizon')
+    parser.add_argument('--avg-block-time', default=5, help='Average block time. Controls the time delay between every spam round and the one just after that')
 
     return parser.parse_args()
 
@@ -36,11 +34,11 @@ def load_spam_rounds(path):
     return spam_rounds
 
 
-async def spam(endpoints, submit_to_horizon, spam_rounds):
+async def spam(endpoints, submit_to_horizon, spam_rounds, avg_block_time):
     """Receive transaction XDR objects and submit them when their expected ledger arrives."""
     futurs = []
     for rnd, tx_metadata in enumerate(spam_rounds):
-        f = spam_round(tx_metadata, endpoints, submit_to_horizon, rnd)
+        f = spam_round(tx_metadata, endpoints, submit_to_horizon, rnd, avg_block_time)
 
         futurs.append(f)
 
@@ -48,12 +46,12 @@ async def spam(endpoints, submit_to_horizon, spam_rounds):
     return results
 
 
-async def spam_round(tx_metadata, endpoints, submit_to_horizon, rnd):
+async def spam_round(tx_metadata, endpoints, submit_to_horizon, rnd, avg_block_time):
     """Receive transaction XDR objects for a specific future ledger and submit them when that ledger time arrives."""
     xdrs = [tx['xdr'] for tx in tx_metadata.values()]
 
     # start the spam round only when the expected ledger time arrives
-    await asyncio.sleep(rnd * AVG_BLOCK_TIME)
+    await asyncio.sleep(rnd * avg_block_time)
 
     # submit transactions
     #
@@ -120,7 +118,8 @@ async def main():
     results = await spam(
         args.endpoint,
         submit_to_horizon=(not args.submit_to_core),
-        spam_rounds=spam_rounds)
+        spam_rounds=spam_rounds,
+        avg_block_time=args.avg_block_time)
     logging.info('done spamming')
 
     logging.info('writing transaction results to file')
