bd0eda478e680b6dee70cf8f0793c92e57df06bc
Ory Band <oryband@gmail.com>
2019-02-27 18:12:46 +0200

    add --extrachecks and some addition CFLAGS and CXXFLAGS to core build script

--- images/dockerfiles/stellar-core-build/build.sh
+++ images/dockerfiles/stellar-core-build/build.sh
@@ -5,8 +5,10 @@ set -x
 cd /stellar-core
 
 # prepare
+export CFLAGS="-O2 -g1"
+export CXXFLAGS="-w -O2 -g1"
 ./autogen.sh
-./configure --enable-asan --enable-sdfprefs CXXFLAGS=-w
+./configure --enable-asan --enable-extrachecks --enable-sdfprefs
 
 # make format
 
