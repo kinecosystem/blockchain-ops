a88ece26e5c3f32a5c1aa22aa6d6e335a9ccc7ad
Ory Band <oryband@gmail.com>
2019-02-28 13:09:42 +0200

    explicitly set C and CXX env vars

--- images/dockerfiles/stellar-core-build/build.sh
+++ images/dockerfiles/stellar-core-build/build.sh
@@ -4,10 +4,11 @@ set -x
 
 cd /stellar-core
 
-# prepare
-export CFLAGS="-O2 -g1"
-export CXXFLAGS="-w -O2 -g1"
+export C=gcc-6 CFLAGS="-O2 -g1"
+export CXX=g++-6 CXXFLAGS="-w -O2 -g1"
+
 ./autogen.sh
+
 ./configure --enable-asan --enable-extrachecks --enable-sdfprefs
 
 # make format
