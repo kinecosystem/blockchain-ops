09a3d857cc6709c6c8b6c51a4b6ba34052fb2799
Ami Blonder <ami.blonder@kik.com>
2019-05-05 17:32:53 +0300

    add poc for athena-bound metrics

--- images/dockerfiles/telegraf-datadog-forwarder/build-and-push.sh
+++ images/dockerfiles/telegraf-datadog-forwarder/build-and-push.sh
@@ -1,2 +1,2 @@
-docker build . -t kinecosystem/telegraf-datadog-forwarder:latest
-docker push kinecosystem/telegraf-datadog-forwarder:latest
+docker build . -t kinecosystem/telegraf-datadog-forwarder:athena-poc
+docker push kinecosystem/telegraf-datadog-forwarder:athena-poc
