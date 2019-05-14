2dfa05b85a5db939c30a1c5372d8a5c37fcf7c0b
Ami Blonder <Ami.Blonder@kik.com>
2019-05-14 12:50:04 +0300

    aggreate horizon statsd metrics over 1m (#162)

--- images/dockerfiles/horizon-telegraf/build-and-push.sh
+++ images/dockerfiles/horizon-telegraf/build-and-push.sh
@@ -1,2 +1,2 @@
-docker build . -t kinecosystem/horizon-telegraf:v1.0.23
-docker push kinecosystem/horizon-telegraf:v1.0.23
+docker build . -t kinecosystem/horizon-telegraf:v1.0.24
+docker push kinecosystem/horizon-telegraf:v1.0.24
