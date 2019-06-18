d791538120a1f35e6e226b650f4107ab5669a18c
Ami Blonder <829266+fuzzyami@users.noreply.github.com>
2019-06-18 10:20:25 +0300

    re-add the queued checkpoints metric, fix broken cloudwatch metric (#175)

--- images/dockerfiles/stellar-core-telegraf/build-and-push.sh
+++ images/dockerfiles/stellar-core-telegraf/build-and-push.sh
@@ -1,3 +1,3 @@
-docker build . -t kinecosystem/stellar-core-telegraf:v1.0.31
-docker push kinecosystem/stellar-core-telegraf:v1.0.31
+docker build . -t kinecosystem/stellar-core-telegraf:v1.0.32
+docker push kinecosystem/stellar-core-telegraf:v1.0.32
 
