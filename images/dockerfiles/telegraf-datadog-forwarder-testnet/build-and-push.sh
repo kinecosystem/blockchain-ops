715e03b38270a800d1be769c8715d2a8c7ba3c33
Ami Blonder <fuzzyami@gmail.com>
2019-06-20 16:35:58 +0300

    comment out some prod-specific metrics on the testnet metrics forwarder

--- images/dockerfiles/telegraf-datadog-forwarder-testnet/build-and-push.sh
+++ images/dockerfiles/telegraf-datadog-forwarder-testnet/build-and-push.sh
@@ -1,2 +1,2 @@
-docker build . -t kinecosystem/telegraf-datadog-forwarder-testnet:v1.0.0
-docker push kinecosystem/telegraf-datadog-forwarder-testnet:v1.0.0
+docker build . -t kinecosystem/telegraf-datadog-forwarder-testnet:v1.0.5
+docker push kinecosystem/telegraf-datadog-forwarder-testnet:v1.0.5
