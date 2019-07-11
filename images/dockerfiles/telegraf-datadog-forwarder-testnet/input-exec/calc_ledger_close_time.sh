095ae64a6e832b2d8291c183539d858ba8f90efd
Yogev Heskia <yogev911@gmail.com>
2019-07-11 16:37:23 +0300

    telegraf input for monitor ledger close time

---
+++ images/dockerfiles/telegraf-datadog-forwarder-testnet/input-exec/calc_ledger_close_time.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+url="https://horizon-testnet.kin.org/ledgers?limit=2&order=desc"
+data=$(curl -s "$url")
+ledger0=$(echo "$data" | jq -r "._embedded.records[0].closed_at")
+ledger1=$(echo "$data" | jq -r "._embedded.records[1].closed_at")
+diff=$(echo $(date --date="$ledger0" +%s) - $(date --date="$ledger1" +%s)| bc)
+printf 'ledger_close_time seconds=%d\n' $diff
