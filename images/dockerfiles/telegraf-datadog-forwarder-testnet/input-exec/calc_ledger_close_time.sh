3af3c6d2ef1236ecd6503aa8ae306d361adc491c
Ami Blonder <829266+fuzzyami@users.noreply.github.com>
2019-07-14 16:46:50 +0300

    turn field into tag in close time metric (#189)

--- images/dockerfiles/telegraf-datadog-forwarder-testnet/input-exec/calc_ledger_close_time.sh
+++ images/dockerfiles/telegraf-datadog-forwarder-testnet/input-exec/calc_ledger_close_time.sh
@@ -5,4 +5,4 @@ data=$(curl -s "$url")
 ledger0=$(echo "$data" | jq -r "._embedded.records[0].closed_at")
 ledger1=$(echo "$data" | jq -r "._embedded.records[1].closed_at")
 diff=$(echo $(date --date="$ledger0" +%s) - $(date --date="$ledger1" +%s) | bc)
-printf 'ledger_close_time stellar_network=testnet seconds=%d\n' $diff
+printf 'ledger_close_time,stellar_network=testnet seconds=%d\n' $diff
