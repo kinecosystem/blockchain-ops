f71b5c0a52ae7db40af57a1b82b53ff7daac34d8
test-name <test@email.com>
2019-07-17 13:45:38 +0300

    New-Metric-Tx-Ops-Count-Telegraf-Forwarder

--- images/dockerfiles/telegraf-datadog-forwarder/input-exec/count_tx_ops.sh
+++ images/dockerfiles/telegraf-datadog-forwarder/input-exec/count_tx_ops.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-url="https://horizon-testnet.kin.org/ledgers?limit=1&order=desc"
+url="http://horizon.kinfederation.com/ledgers?limit=1&order=desc"
 data=$(curl -s "$url")
 tx_count=$(echo "$data" | jq -r "._embedded.records[0].transaction_count")
 ops_count=$(echo "$data" | jq -r "._embedded.records[0].operation_count")
