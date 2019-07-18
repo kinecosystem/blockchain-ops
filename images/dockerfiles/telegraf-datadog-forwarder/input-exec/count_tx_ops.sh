af49498151a16a3fb677d98d7ac18407e273f7c7
Ami <fuzzyami@gmail.com>
2019-07-18 09:15:55 +0300

    correct the stellar_network tag on the tx_count metric

--- images/dockerfiles/telegraf-datadog-forwarder/input-exec/count_tx_ops.sh
+++ images/dockerfiles/telegraf-datadog-forwarder/input-exec/count_tx_ops.sh
@@ -4,4 +4,4 @@ url="http://horizon.kinfederation.com/ledgers?limit=1&order=desc"
 data=$(curl -s "$url")
 tx_count=$(echo "$data" | jq -r "._embedded.records[0].transaction_count")
 ops_count=$(echo "$data" | jq -r "._embedded.records[0].operation_count")
-printf '%s\n' "ledger,stellar_network=testnet tx_count=$tx_count,ops_count=$ops_count"
+printf '%s\n' "ledger,stellar_network=fed tx_count=$tx_count,ops_count=$ops_count"
