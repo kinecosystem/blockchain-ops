ce549c746e3c5ce079a8739507498c5e8cbe72c4
Ami <fuzzyami@gmail.com>
2019-08-01 17:51:15 +0300

    new stellare-core telegraf version

--- images/dockerfiles/stellar-core-telegraf/build-and-push.sh
+++ images/dockerfiles/stellar-core-telegraf/build-and-push.sh
@@ -1,3 +1,3 @@
-docker build . -t kinecosystem/stellar-core-telegraf:v1.0.34
-docker push kinecosystem/stellar-core-telegraf:v1.0.34
+docker build . -t kinecosystem/stellar-core-telegraf:v1.0.35
+docker push kinecosystem/stellar-core-telegraf:v1.0.35
 
