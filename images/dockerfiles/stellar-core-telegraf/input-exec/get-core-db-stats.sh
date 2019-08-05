354532fde02ecd122170e3596514578cff810475
Ami Blonder <829266+fuzzyami@users.noreply.github.com>
2019-08-05 08:35:48 +0300

    measure size of the txhistory table on cores (#205)

--- images/dockerfiles/stellar-core-telegraf/input-exec/get-core-db-stats.sh
+++ images/dockerfiles/stellar-core-telegraf/input-exec/get-core-db-stats.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-txhistorysize=$(psql $CORE_DB_URL -c 'select distinct count(*) from txhistory ;'|sed -n '3p'|tr -d ' ')
+txhistorysize=$(psql $CORE_DB_URL -c "select pg_relation_size('txhistory');"|sed -n '3p'|tr -d ' ')
 printf 'core_db_stats txhistorysize=%d\n' $txhistorysize
