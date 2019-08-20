789c2a5bfd847e8d9c9b05a3c3d64b5dd7158631
Ittiel <ittiel@gmail.com>
2019-08-20 08:33:25 +0300

    Update build-and-push.sh
    Upgraded version number as appears on docker-hub

--- images/dockerfiles/horizon-telegraf/build-and-push.sh
+++ images/dockerfiles/horizon-telegraf/build-and-push.sh
@@ -1,2 +1,2 @@
-docker build . -t kinecosystem/horizon-telegraf:v1.0.27
-docker push kinecosystem/horizon-telegraf:v1.0.27
+docker build . -t kinecosystem/horizon-telegraf:v1.0.28
+docker push kinecosystem/horizon-telegraf:v1.0.28
